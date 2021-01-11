8b784c40e591e15848c6288bd76c5ac9d1c4b471
Israel Garcia <israel.garcia@societao.com>
2021-01-11 00:53:10 -0700

    update

--- zoom-records.py
+++ zoom-records.py
@@ -28,7 +28,7 @@ def get_zoom_users():
 	return users
 
 def get_zoom_files(users):
-	print('::::::::::::::::::::::::::::::Downloading meeting files::::::::::::::::::::::::::::::')
+	print('\n::::::::::::::::::::::::::::::Downloading meetings list::::::::::::::::::::::::::::::')
 
 	url = "https://api.zoom.us/v2/users/"
 	query = {"trash_type":"meeting_recordings","mc":"false","page_size":"100"}
@@ -37,15 +37,16 @@ def get_zoom_files(users):
 	records_list = []
 
 	for user in users['users']:
+		_url = url + user['id']+'/recordings'
 		from_date = datetime.strptime(_from_date, '%Y-%m-%d').date()  #date(2020, 01, 01)
 		to_date = from_date.replace(month=from_date.month+1)
 
+		print('\n::::::::::::::::::::::::::::::'+_url+'::::::::::::::::::::::::::::::')
 		while from_date < end_date:
 			query["from"] = str(from_date)
 			query["to"] = str(to_date)
-			_url = url + user['id']+'/recordings'
 
-			print('::::::::::::::::::::::::::::::'+_url+'::::::::::::::::::::::::::::::')
+			print('::::::::::::::::::::::::::::::['+str(from_date)+'] - ['+str(to_date)+']::::::::::::::::::::::::::::::')
 
 			response = requests.request("GET", _url, headers=headers, params=query)
 			json_response = json.loads(response.content)
@@ -54,19 +55,20 @@ def get_zoom_files(users):
 				for meeting in json_response['meetings']:
 					if meeting['recording_count'] > 0:
 						for recording in meeting['recording_files']:
-							if recording['file_extension'] == 'MP4':
-								item = {}
-								item['username'] = user['first_name']
-								item['recording_start'] = recording['recording_start']
-								item['recording_end'] = recording['recording_end']
-								item['download_url'] = recording['download_url']
-								item['topic'] = meeting['topic']
-								item['record_id'] = recording['id']
-								item['meeting_id'] = meeting['id']
-								item['file_type'] = 'mp4'
+#							if recording['file_extension'] == 'MP4':
+							item = {}
+							item['username'] = user['first_name'].encode('utf-8')
+							item['recording_start'] = recording['recording_start']
+							item['recording_end'] = recording['recording_end']
+							item['download_url'] = recording['download_url'].encode('utf-8')
+							item['play_url'] = recording['play_url'].encode('utf-8')
+							item['topic'] = meeting['topic'].encode('utf-8')
+							item['record_id'] = recording['id']
+							item['meeting_id'] = meeting['id']
+							item['file_type'] = 'mp4'
 
-								print(item['topic'])
-								records_list.append(item)
+#								print(item['topic'])
+							records_list.append(item)
 
 			if to_date.month+1 > 12:
 				to_date = to_date.replace(year=to_date.year+1)
@@ -83,30 +85,27 @@ def get_zoom_files(users):
 	return records_list
 
 def download_zoom_files(records_list):
+	print('\n::::::::::::::::::::::::::::::Downloading meetings files::::::::::::::::::::::::::::::')
 	for index, record in enumerate(records_list):
-		if not os.path.exists('./meetings/'+record['username'].encode('utf-8')+'/'+record['topic'].encode('utf-8')):
-			os.makedirs('./meetings/'+record['username'].encode('utf-8')+'/'+record['topic'].encode('utf-8'))
-		filename = './meetings/'+record['username'].encode('utf-8')+'/'+record['topic'].encode('utf-8')+'/'+'record-'+str(index)+'.'+record['file_type']
+		if not os.path.exists('./meetings/'+record['username']+'/'+record['topic']):
+			os.makedirs('./meetings/'+record['username']+'/'+record['topic'])
+
+		filename = './meetings/'+record['username']+'/'+record['topic']+'/'+ record['download_url'].encode('utf-8').split("/")[-1] +'.'+record['file_type']
+		record['filename']=filename
 		wget.download(record['download_url'],filename)
 		record["status"]="downloaded"
 	return records_list
 
 def save_csv(filename, fileobject):
-	for index, record in enumerate(fileobject):
-		if not os.path.exists('./meetings/'+record['username'].encode('utf-8')+'/'+record['topic'].encode('utf-8')):
-			os.makedirs('./meetings/'+record['username'].encode('utf-8')+'/'+record['topic'].encode('utf-8'))
-		filename = './meetings/'+record['username'].encode('utf-8')+'/'+record['topic'].encode('utf-8')+'/'+'record-'+str(index)+'.'+record['file_type']
-		wget.download(record['download_url'],filename)
-		record["status"]="downloaded"
-
-	mfilename='./records.csv'
-	with open(mfilename, 'w') as f:
+	with open(filename, 'w') as f:
 		writer = csv.writer(f)
-		writer.writerow(["RECORDID", "MEETINGID", "TOPIC", "STATUS", "URL", "START", "END"])
+		writer.writerow(["RECORDID", "MEETINGID", "TOPIC","FILENAME", "STATUS", "URL","PLAY URL", "START", "END"])
 		for item in fileobject:
-			writer.writerow([item['record_id'], item['meeting_id'], item['topic'].encode('utf-8'), item['status'], item['download_url'], item['recording_start'], item['recording_end']])
+			writer.writerow([item['record_id'], item['meeting_id'], item['topic'], item['filename'], item['status'], item['download_url'], item['play_url'], item['recording_start'], item['recording_end']])
 
-
+csv_file = './records-'+str(time())+'.csv'
 users = get_zoom_users()
 recordings = get_zoom_files(users)
-print(recordings)
+downloaded_records = download_zoom_files(recordings)
+save_csv(csv_file,downloaded_records)
+#print(recordings)
