aa198ad182da83c7c606ec7f67c6a1dbdcb45874
Israel Garcia <israel.garcia@societao.com>
2021-02-01 15:13:48 -0700

    Fix

--- zoom_files_delete.py
+++ zoom_files_delete.py
@@ -12,16 +12,19 @@ def delete_zoom_files(records):
 	query = {"action":"trash"}
 	headers = {'authorization': 'Bearer '+utils.zoom_token}
 
+	print('\n'+' Moving to trash files '.center(100,':'))
 	for record in records:
 		url = "https://api.zoom.us/v2/meetings/{meeting_id}/recordings/{record_id}"
 		url=url.format( meeting_id=urllib.parse.quote(record['meeting_uuid']),record_id=urllib.parse.quote(record['record_id']) )
 
-		if utils.input_type == 2 and (record['status']=='downloaded' or record['vimeo_status']=='available'):
-			print('::::::::::::::::::::::::::::::Deleting file {recordid} in meeting {meetingid}::::::::::::::::::::::::::::::'.format(recordid=record['record_id'], meetingid=record['meeting_id']) )
-			response = requests.request("DELETE", url, headers=headers, params=query)
-			if response.status_code != 204:
-				json_response = json.loads(response.content)
-				print(json_response)
+		#if utils.input_type == 2 and (record['status']=='downloaded' or record['vimeo_status']=='available'):
+		response = requests.request("DELETE", url, headers=headers, params=query)
+		if response.status_code != 204:
+			json_response = json.loads(response.content)
+			print(json_response)
+		else:
+			print(' Deleting file {filename} in meeting {meetingid} '.format(filename=record['file_name'], meetingid=record['meeting_id']).center(100,':'))
+			record['status']='deleted'
 
 	return records
 
@@ -29,3 +32,4 @@ if __name__ == "__main__":
 	utils = Utils()
 	files = utils.get_records(sys.argv, 'zoom_files_delete.py')
 	files = delete_zoom_files(files)
+	utils.save_csv(files, utils.output_file)
